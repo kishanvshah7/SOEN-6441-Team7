d1557500f8c0b83ea2a6c40d71d5608d7fba04e6
rahulk-kikani <rahulk.kikani@gmail.com>
2014-11-26 00:40:16 -0500

    XML Reader for Game Load added

--- src/extra/ReadXMLFile.java
+++ src/extra/ReadXMLFile.java
@@ -10,54 +10,107 @@ package extra;
  *
  * @author Rahul K Kikani
  */
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.DocumentBuilder;
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Node;
-import org.w3c.dom.Element;
 import java.io.File;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
  
 public class ReadXMLFile {
  
-  public static void main(String argv[]) {
+  public static void main(String[] args) {
  
     try {
  
-	File fXmlFile = new File("SavedGame/demo.xml");
-	DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-	DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-	Document doc = dBuilder.parse(fXmlFile);
+	File file = new File("SavedGame/Game1.xml");
  
-	doc.getDocumentElement().normalize();
+	DocumentBuilder dBuilder = DocumentBuilderFactory.newInstance()
+                             .newDocumentBuilder();
+ 
+	Document doc = dBuilder.parse(file);
  
 	System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
  
-	NodeList nList = doc.getElementsByTagName("staff");
- 
-	System.out.println("----------------------------");
- 
-	for (int temp = 0; temp < nList.getLength(); temp++) {
- 
-		Node nNode = nList.item(temp);
- 
-		System.out.println("\nCurrent Element :" + nNode.getNodeName());
- 
-		if (nNode.getNodeType() == Node.ELEMENT_NODE) {
- 
-			Element eElement = (Element) nNode;
- 
-			System.out.println("Staff id : " + eElement.getAttribute("id"));
-			System.out.println("First Name : " + eElement.getElementsByTagName("firstname").item(0).getTextContent());
-			System.out.println("Last Name : " + eElement.getElementsByTagName("lastname").item(0).getTextContent());
-			System.out.println("Nick Name : " + eElement.getElementsByTagName("nickname").item(0).getTextContent());
-			System.out.println("Salary : " + eElement.getElementsByTagName("salary").item(0).getTextContent());
- 
-		}
+	if (doc.hasChildNodes()) {
+            printNote(doc.getChildNodes());
 	}
+ 
     } catch (Exception e) {
-	e.printStackTrace();
+	System.out.println(e.getMessage());
     }
+ 
+  }
+ 
+  private static void printNote(NodeList nodeList) {
+      nodeList = nodeList.item(0).getChildNodes();
+    for (int count = 0; count < nodeList.getLength(); count++) {
+	Node tempNode = nodeList.item(count);
+	if (tempNode.getNodeType() == Node.ELEMENT_NODE) {
+		
+		System.out.println("\nNode Name =" + tempNode.getNodeName() + " [OPEN]");
+		//System.out.println("Node Value =" + tempNode.getTextContent());
+		
+                if (tempNode.getNodeName().equals("mapinfo") && tempNode.hasChildNodes()) {
+                    NodeList xnodeList = tempNode.getChildNodes();
+                    for (int x = 0; x < xnodeList.getLength(); x++) {
+                        Node xtempNode = xnodeList.item(x);
+                        if (xtempNode.hasAttributes()) {
+                            Element eElement = (Element) xtempNode;
+                            System.out.println("X: "+eElement.getAttribute("x"));
+                            System.out.println("Y: "+eElement.getAttribute("y"));
+                        }
+                        
+                        NodeList tilenodeList = xtempNode.getChildNodes();
+                        for (int j = 0; j < tilenodeList.getLength(); j++) {
+                            Node jtempNode = tilenodeList.item(j);
+                            if (jtempNode.hasAttributes()) {
+                                Element eElement = (Element) jtempNode;
+                                System.out.println("("+eElement.getAttribute("x")+","+eElement.getAttribute("x")+") = "+eElement.getAttribute("value"));
+                            }
+                        }
+                    }
+		} else if(tempNode.getNodeName().equals("towerinfo")){
+                    NodeList towerList = tempNode.getChildNodes();
+                    for (int x = 0; x < towerList.getLength(); x++) {
+                        Node towerNode = towerList.item(x);
+                        if (towerNode.hasAttributes()) {
+                            Element eElement = (Element) towerNode;
+                            System.out.println("ID: "+eElement.getAttribute("id"));
+                            System.out.println("Level: "+eElement.getAttribute("level"));
+                        }
+                    }
+                } else if(tempNode.getNodeName().equals("gameinfo")){
+                    NodeList towerList = tempNode.getChildNodes();
+                    for (int x = 0; x < towerList.getLength(); x++) {
+                        Node towerNode = towerList.item(x);
+                        if(!towerNode.getNodeName().equals("#text"))
+                        {
+                            System.out.println("Name: "+towerNode.getNodeName());
+                            System.out.println("Val: "+towerNode.getTextContent());
+                        }
+                    }
+                } else if(tempNode.getNodeName().equals("loginfo")){
+                    NodeList towerList = tempNode.getChildNodes();
+                    for (int x = 0; x < towerList.getLength(); x++) {
+                        Node towerNode = towerList.item(x);
+                        if(!towerNode.getNodeName().equals("#text"))
+                        {
+                            Element eElement = (Element) towerNode;
+                            System.out.println("time: "+eElement.getAttribute("time"));
+                            System.out.println("type: "+eElement.getAttribute("type"));
+                            System.out.println("Name: "+towerNode.getNodeName());
+                            System.out.println("Val: "+towerNode.getTextContent());
+                        }
+                    }
+                }
+		System.out.println("Node Name =" + tempNode.getNodeName() + " [CLOSE]");
+	}
+ 
+    }
+ 
   }
  
 }
